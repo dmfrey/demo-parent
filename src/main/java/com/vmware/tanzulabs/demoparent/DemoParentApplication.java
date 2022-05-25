b8e117cd744553ff6d2be485fb5261b5111f52c8
Daniel Frey <dmfrey@gmail.com>
2022-05-25 11:11:03 -0400

    initial commit

---
+++ src/main/java/com/vmware/tanzulabs/demoparent/DemoParentApplication.java
@@ -0,0 +1,13 @@
+package com.vmware.tanzulabs.demoparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoParentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vmware/tanzulabs/demoparent/DemoParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmware.tanzulabs.demoparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
