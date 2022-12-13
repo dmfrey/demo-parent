dfdd841fc8660b215d2ae01c86b1fa45d30ef026
Daniel Frey <dmfrey@gmail.com>
2022-12-13 16:51:32 -0700

    updating code

---
+++ app-parent/person-app/src/main/java/com/vmware/tanzulabs/app/annotations/usecase/SelfValidating.java
@@ -0,0 +1,27 @@
+package com.vmware.tanzulabs.app.annotations.usecase;
+
+import javax.validation.*;
+import java.util.Set;
+
+public abstract class SelfValidating<T> {
+
+    private final Validator validator;
+
+    protected SelfValidating() {
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+
+    }
+
+    protected void validateSelf() {
+
+        Set<ConstraintViolation<T>> violations = validator.validate((T) this);
+        if( !violations.isEmpty() ) {
+
+            throw new ConstraintViolationException( violations );
+        }
+
+    }
+
+}
