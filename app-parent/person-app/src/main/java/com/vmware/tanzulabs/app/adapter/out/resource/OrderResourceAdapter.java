29ca079e3f1be9ef6ff1546a95a5f2bdbc06c57d
Daniel Frey <dmfrey@gmail.com>
2023-01-11 16:46:33 -0500

    enable circuit breaker

--- app-parent/person-app/src/main/java/com/vmware/tanzulabs/app/adapter/out/resource/OrderResourceAdapter.java
+++ app-parent/person-app/src/main/java/com/vmware/tanzulabs/app/adapter/out/resource/OrderResourceAdapter.java
@@ -2,6 +2,8 @@ package com.vmware.tanzulabs.app.adapter.out.resource;
 
 import com.vmware.tanzulabs.app.application.out.LookupOrdersByCustomerIdPort;
 import com.vmware.tanzulabs.app.domain.Order;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -15,14 +17,17 @@ import java.util.stream.Stream;
 class OrderResourceAdapter implements LookupOrdersByCustomerIdPort {
 
     private final RestTemplate restTemplate;
+    private final CircuitBreakerFactory circuitBreakerFactory;
     private final OrderResourceConfigurationProperties configurationProperties;
 
     OrderResourceAdapter(
             final RestTemplate restTemplate,
+            final CircuitBreakerFactory circuitBreakerFactory,
             final OrderResourceConfigurationProperties configurationProperties
     ) {
 
         this.restTemplate = restTemplate;
+        this.circuitBreakerFactory = circuitBreakerFactory;
         this.configurationProperties = configurationProperties;
 
     }
@@ -30,9 +35,14 @@ class OrderResourceAdapter implements LookupOrdersByCustomerIdPort {
     @Override
     public Optional<List<Order>> lookup( String customerId ) {
 
-        ResponseEntity<OrderResource[]> resourceResponseEntity = this.restTemplate.getForEntity( configurationProperties.orderResourceUrl() + "/orders/{customerId}", OrderResource[].class, customerId );
+        var resourceResponseEntity =
+                this.circuitBreakerFactory.create( "order-app" )
+                        .run(
+                            () -> this.restTemplate.getForEntity( configurationProperties.orderResourceUrl() + "/orders/{customerId}", OrderResource[].class, customerId ),
+                            throwable -> new ResponseEntity<>( new OrderResource[] {}, HttpStatus.GATEWAY_TIMEOUT )
+                        );
 
-        List<Order> orders = Stream.of( Objects.requireNonNull(resourceResponseEntity.getBody() ) )
+        var orders = Stream.of( Objects.requireNonNull( resourceResponseEntity.getBody() ) )
                 .map( orderResource -> new Order( orderResource.orderId(), orderResource.customerId() ) )
                 .toList();
 
--- app-parent/person-app/src/test/java/com/vmware/tanzulabs/app/adapter/out/resource/OrderResourceAdapterTests.java
+++ app-parent/person-app/src/test/java/com/vmware/tanzulabs/app/adapter/out/resource/OrderResourceAdapterTests.java
@@ -1,28 +1,37 @@
 package com.vmware.tanzulabs.app.adapter.out.resource;
 
 import com.vmware.tanzulabs.app.domain.Order;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.timelimiter.TimeLimiterRegistry;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigurationProperties;
 import org.springframework.cloud.contract.stubrunner.junit.StubRunnerExtension;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
 import java.util.*;
 
 import static com.vmware.tanzulabs.app.adapter.SpringCloudContractHelper.repoRoot;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.spy;
 
-@SpringBootTest(
-        classes = {
-                OrderResourceConfiguration.class,
-                OrderResourceAdapter.class
-        },
-        properties = {
-                "webservice.order-resource-url=http://localhost:8099"
-        }
-)
 class OrderResourceAdapterTests {
+
+    static Resilience4JCircuitBreakerFactory circuitBreakerFactory = spy(
+            new Resilience4JCircuitBreakerFactory(
+                    CircuitBreakerRegistry.ofDefaults(),
+                    TimeLimiterRegistry.ofDefaults(),
+                    null,
+                    new Resilience4JConfigurationProperties()
+            ));
 
     static Map<String, String> contractProperties() {
 
@@ -40,9 +49,6 @@ class OrderResourceAdapterTests {
             .withMappingsOutputFolder( "target/outputmappings" )
             .withProperties( contractProperties() );
 
-    @Autowired
-    OrderResourceAdapter subject;
-
     private final String fakeCustomerId = "1";
     private final String fakeCustomerIdNotFound = "2";
     private final UUID fakeOrderId = UUID.fromString( "f803c8fc-38ee-4949-8829-2c03d364d3ac" );
@@ -50,22 +56,82 @@ class OrderResourceAdapterTests {
     @Test
     void when_customerId_one_verify_orders() {
 
-        var actual = this.subject.lookup( fakeCustomerId );
+        try( ConfigurableApplicationContext context = new SpringApplicationBuilder()
+                .web( WebApplicationType.NONE )
+                .sources( TestConfiguration.class )
+                .run(
+                        "--webservice.order-resource-url=http://localhost:8099"
+                )
+        ) {
 
-        var expected = Optional.of( List.of( new Order( fakeOrderId, fakeCustomerId ) ) );
+            var subject = context.getBean( OrderResourceAdapter.class );
 
-        assertThat( actual ).isEqualTo( expected );
+            var actual = subject.lookup( fakeCustomerId );
+
+            var expected = Optional.of( List.of( new Order( fakeOrderId, fakeCustomerId ) ) );
+
+            assertThat( actual ).isEqualTo( expected );
+
+        }
 
     }
 
     @Test
     void when_customerId_two_verify_no_orders() {
 
-        var actual = this.subject.lookup( fakeCustomerIdNotFound );
+        try( ConfigurableApplicationContext context = new SpringApplicationBuilder()
+                .web( WebApplicationType.NONE )
+                .sources( TestConfiguration.class )
+                .run(
+                        "--webservice.order-resource-url=http://localhost:8099"
+                )
+        ) {
 
-        var expected = Optional.of( Collections.emptyList() );
+            var subject = context.getBean( OrderResourceAdapter.class );
 
-        assertThat( actual ).isEqualTo( expected );
+            var actual = subject.lookup( fakeCustomerIdNotFound );
+
+            var expected = Optional.of( Collections.emptyList() );
+
+            assertThat( actual ).isEqualTo( expected );
+
+        }
+
+    }
+
+    @Test
+    void when_order_app_down_verify_no_orders() {
+
+        try( ConfigurableApplicationContext context = new SpringApplicationBuilder()
+                .web( WebApplicationType.NONE )
+                .sources( TestConfiguration.class )
+                .run(
+                        "--webservice.order-resource-url=http://localhost:9999"
+                )
+        ) {
+
+            var subject = context.getBean( OrderResourceAdapter.class );
+
+            var actual = subject.lookup( fakeCustomerIdNotFound );
+
+            var expected = Optional.of( Collections.emptyList() );
+
+            assertThat( actual ).isEqualTo( expected );
+
+        }
+
+    }
+
+    @SpringBootConfiguration
+    @EnableAutoConfiguration
+    @Import({ OrderResourceConfiguration.class, OrderResourceAdapter.class })
+    protected static class TestConfiguration {
+
+        @Bean
+        Resilience4JCircuitBreakerFactory circuitBreakerFactory() {
+
+            return circuitBreakerFactory;
+        }
 
     }
 
