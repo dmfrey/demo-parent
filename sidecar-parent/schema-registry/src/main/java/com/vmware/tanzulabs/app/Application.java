dbf53db5edc1dd8260c0616977a33b7eb6b3a74e
Daniel Frey <dmfrey@gmail.com>
2023-01-25 12:00:05 -0500

    add schema registry sidecar

---
+++ sidecar-parent/schema-registry/src/main/java/com/vmware/tanzulabs/app/Application.java
@@ -0,0 +1,17 @@
+package com.vmware.tanzulabs.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.schema.registry.EnableSchemaRegistryServer;
+
+@SpringBootApplication
+@EnableSchemaRegistryServer
+public class Application {
+
+	public static void main( String[] args ) {
+
+		SpringApplication.run( Application.class, args );
+
+	}
+
+}
